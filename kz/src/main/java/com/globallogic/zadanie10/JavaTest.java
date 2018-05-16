2fc8d0095ac373bc276b570007496f99d216fdab
Kz <Kz@komp>
2018-05-16 20:37:15 +0200

    zadanie 10 wyswietlanie tablicy  i exception

---
+++ kz/src/main/java/com/globallogic/zadanie10/JavaTest.java
@@ -0,0 +1,11 @@
+package com.globallogic.zadanie10;
+
+public class JavaTest {
+	private static int[] a = {1, 2, 3, 4, 5};
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.print(a[i]);
+		}
+	}
+}
