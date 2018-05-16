ae30a6719c1408c6fd93fc9c3fc9507ae2aaf7b3
Kz <Kz@komp>
2018-05-16 20:41:54 +0200

    zadanie 11 ifelse z błędem

---
+++ kz/src/main/java/com/globallogic/zadanie11/JavaTest.java
@@ -0,0 +1,14 @@
+package com.globallogic.zadanie11;
+
+public class JavaTest {
+
+	public static void main(String[] args) {
+		int i = 3;
+		if (i) {
+			System.out.println("if");
+		} else {
+			System.out.println("else");
+		}
+	}
+
+}
